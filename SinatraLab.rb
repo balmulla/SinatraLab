45220a7dd1a4409b22d4b4f15e49e81934bb2d1e
bmulla <bmulla@qatar.cmu.edu>
2019-01-30 18:13:04 +0000

    Edited application.erb and Sinatralab.rb

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 get '/' do
-  erb :index
+  erb :application do
+    erb :index
+  end
 end
 
 post '/' do
@@ -11,5 +13,8 @@ post '/' do
     @class_love = params[:class_love]
     @feedback = params[:feedback]
     @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefully you'll REALLY love it by the end"
-    erb :response
+
+    erb :application do
+      erb :response
+    end
 end

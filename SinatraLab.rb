6cda7240cc12d27fd94f12c7fa4131b3bf82c7a3
bmulla <bmulla@qatar.cmu.edu>
2019-01-30 17:57:24 +0000

    created application.erb and response.erb

--- SinatraLab.rb
+++ SinatraLab.rb
@@ -2,3 +2,14 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+post '/' do
+#    "Hello World!"
+
+    @fname = params[:firstname]
+    @lname = params[:lastname]
+    @class_love = params[:class_love]
+    @feedback = params[:feedback]
+    @happy = (@class_love == "More than I care to say") ? "That's Great!" : "Well, hopefully you'll REALLY love it by the end"
+    erb :response
+end
